70c6a026d841a79efd5ad624382c9d6c11b22524
Robert Grigoryan <dev.robert.grigoryan@gmail.com>
2019-11-03 19:54:09 +0300

    feat (prettier): add prettier task

--- tasks/build-css.js
+++ tasks/build-css.js
@@ -3,7 +3,7 @@ const sass = require("gulp-sass");
 const sourcemaps = require("gulp-sourcemaps");
 const minify = require("gulp-clean-css");
 const glop = require("gulp-sass-glob");
-const alias = require('gulp-style-aliases');
+const alias = require("gulp-style-aliases");
 const server = require("browser-sync");
 
 const { paths } = require("./constants");
@@ -11,8 +11,8 @@ const { paths } = require("./constants");
 module.exports = function() {
   const aliases = {
     "~normalize.css": "./node_modules/normalize.css/normalize.css",
-    "components": "./src/components/**/*.scss",
-    "common": "./src/common/**/*.scss"
+    components: "./src/components/**/*.scss",
+    common: "./src/common/**/*.scss"
   };
 
   return src(paths.src.css)
